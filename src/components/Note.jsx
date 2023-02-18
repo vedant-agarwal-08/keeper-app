e1405811efd50a65386199f1439bb671951e3f9a
vedant-agarwal-08 <105448072+vedant-agarwal-08@users.noreply.github.com>
2023-02-18 23:37:05 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note(){
+    return(
+    <div className="note">
+        <h1>This is the titles</h1>
+        <p> This is a paragraph</p>
+    </div>
+    );
+}
+
+export default Note;
